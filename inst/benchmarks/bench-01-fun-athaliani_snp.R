3b3f4ccb4536959d31cd714f4899d318b58e7f42
variani <machovariani@gmail.com>
2016-10-16 00:07:34 +0200

    up

--- inst/benchmarks/bench-01-fun-athaliani_snp.R
+++ inst/benchmarks/bench-01-fun-athaliani_snp.R
@@ -10,16 +10,24 @@ times <- 5
 ### small `n`
 n <- 500
 
-out <- microbenchmark(
+out1 <- microbenchmark(
   dplyr = athaliana_snp(method = "dplyr", n = n),
   fread = athaliana_snp(method = "fread", n = n),
   bycol = athaliana_snp(method = "bycol", n = n),
   times = times)
   
 ### moderate `n`
-n <- 2e3
+n <- 5e3
 
-out <- microbenchmark(
+out2 <- microbenchmark(
   dplyr = athaliana_snp(method = "dplyr", n = n),
   bycol = athaliana_snp(method = "bycol", n = n),
   times = times)  
+  
+### relatevely large `n`
+n <- 10e3
+
+out3 <- microbenchmark(
+  dplyr = athaliana_snp(method = "dplyr", n = n),
+  bycol = athaliana_snp(method = "bycol", n = n),
+  times = times)    
